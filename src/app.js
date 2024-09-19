68a15c985e47eab52633efb42972d8bbedfaa67a
Akshaykumar-2001 <akshaykanugula007@gmail.com>
2024-09-19 09:46:08 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,43 @@
 const express = require("express");
 const app = express();
 
-app.use("/test2", (req, res) => {
-  res.send("Hello ");
-});
+//hangs
+// app.get("/test", (req, res) => {
 
-app.use("/test", (req, res) => {
-  res.send("hellow from test");
+// });
+
+// error : Cannot GET /test
+// app.get("/test", (req, res, next) => {
+//   next();
+// });
+
+// 2nd request handler not used as it is not invoked using next()
+// app.get(
+//   "/test",
+//   (req, res, next) => {
+//     next();
+//   },
+//   (req, res) => {
+//     res.send("respone");
+//   },
+//   (req, res) => {}
+// );
+app.use("/user/login", (req, res) => {
+  res.send("res not sended");
+});
+app.use("/user", (req, res, next) => {
+  const token = "xyz";
+  const flag = "xyz" === token;
+  if (flag) {
+    console.log("auth checked****");
+    // res.send("yes");
+    next();
+  } else {
+    res.status(401).send("unauthorized");
+  }
+});
+app.get("/user/data", (req, res) => {
+  res.send("data");
 });
 
 app.listen(3000, () => {
