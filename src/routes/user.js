be680d8ff55b9cb14fddb51a5605b739cf74b64c
Akshaykumar-2001 <akshaykanugula007@gmail.com>
2024-11-04 16:34:23 +0530

    user connection API

--- src/routes/user.js
+++ src/routes/user.js
@@ -9,10 +9,46 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     const requests = await ConnectionRequest.find({
       toUserId: loggedInUser,
       status: "interested",
-    });
+    }).populate("fromUserId", [
+      "firstName",
+      "lastName",
+      "age",
+      "photoUrl",
+      "gender",
+    ]);
     res.send(requests);
   } catch (err) {
     res.status(400).send("ERROR " + err.message);
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connections = await ConnectionRequest.find({
+      $or: [
+        { fromUserId: loggedInUser._id, status: "accepted" },
+        { toUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", "firstName age gender")
+      .populate("toUserId", "firstName age gender");
+
+    /*
+    - above query we are searched for either fromUserId or toUserId 
+    - what if from user id is us and status is accepted we are sending back fromUser id
+    */
+    const connectionsData = connections.map((connection) => {
+      if (
+        connection.fromUserId._id.toString() === loggedInUser._id.toString()
+      ) {
+        return connection.toUserId;
+      }
+      return connection.fromUserId;
+    });
+    res.send(connectionsData);
+  } catch (err) {
+    res.status(400).send("ERROR" + err.message);
   }
 });
 
